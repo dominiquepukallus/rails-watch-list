a89c867ac061e95f96e7b87d16a7e796a6f97d8e
Dom Pukallus <dpukallus9@gmail.com>
2026-05-21 15:49:27 +1000

    completed routing & controllers for watch list. Yet to apply CSS

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,34 @@
 class ListsController < ApplicationController
-  # before_action :set_list
+  before_action :set_list, only: [:show, :create]
 
   def index
     @lists = List.all
   end
 
+  def show
+
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path, notice: "List saved"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
-  # def set_list
-  #   @list = List.find(params[:id])
-  # end
+  def list_params
+    params.require(:list).permit(:name)
+  end
 
+  def set_list
+    @list = List.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "lists#index"
-  resources :lists 
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
